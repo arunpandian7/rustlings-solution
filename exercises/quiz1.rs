7dc28f6fbeb33005dcbf30349d92aeaa2b62f70e
Arun Pandian R <arunpandian.srk@gmail.com>
2024-06-02 10:49:20 +0530

    progress: 2 june 2024

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (quantity: i8) -> i8 {
+    let unit_price = if quantity > 40 {
+        1
+    } else {
+        2
+    };
+
+    (quantity * unit_price)
+}
 
 // Don't modify this function!
 #[test]
